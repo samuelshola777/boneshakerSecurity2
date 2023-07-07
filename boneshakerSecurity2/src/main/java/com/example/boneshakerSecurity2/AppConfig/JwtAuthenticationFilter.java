58f823b39fc98c0d6df3dfd32614ac0a2c8e4b9b
samuelshola777 <samuelshola14@gmail.com>
2023-07-07 22:29:58 +0100

    first commit

---
+++ boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/AppConfig/JwtAuthenticationFilter.java
@@ -0,0 +1,7 @@
+package com.example.boneshakerSecurity2.AppConfig;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtAuthenticationFilter {
+}
--- boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/model/User.java
+++ boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/model/User.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "_user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
