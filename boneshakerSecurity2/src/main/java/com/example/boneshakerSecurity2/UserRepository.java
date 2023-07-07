aff86ee08c10e4792bb5f06eb86272f570e7552e
samuelshola777 <samuelshola14@gmail.com>
2023-07-07 22:50:44 +0100

    first commit

--- boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/UserRepository.java
+++ boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
 
     @Override
-    Optional<User> findByEmail(Long aLong);
+    Optional<User> findByEmail(String email);
 }
