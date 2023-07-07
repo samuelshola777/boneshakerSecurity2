71980da791fb72b2629ea17dee694cd68d00289e
samuelshola777 <samuelshola14@gmail.com>
2023-07-07 22:56:01 +0100

    first commit

--- boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/AppConfig/JwtAuthenticationFilter.java
+++ boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/AppConfig/JwtAuthenticationFilter.java
@@ -4,12 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@AllArgsConstructor
 @Configuration
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
