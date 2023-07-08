7a1cafa3ec77e696322b58178b863da6adbbc244
samuelshola777 <samuelshola14@gmail.com>
2023-07-08 14:59:05 +0100

    creating the jwt service to extract user name

--- boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/AppConfig/JwtAuthenticationFilter.java
+++ boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/AppConfig/JwtAuthenticationFilter.java
@@ -18,21 +18,38 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-// TODO: when we send a request we need to pass the authentication token withen the header of the request
+
+        // TODO: when we send a request we need to pass the authentication token withen the header of the request
         //TODO : to check we our request have jwt authentication token
        //TODO : trying to extract the header called authorization
+
+
         final String authenticationHeader = request.getHeader("Authorization");
         final String jwt;
+
+
+        // TODO: we have to extract the user email from our Jwt authentication token
+        final String userEmail;
+
+
         //TODO: we want to check if out jwt authentication token header is null or
     //    TODO: start with the keyword string Bearer and a space
+
+
         if (authenticationHeader == null ||! authenticationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
+
+
     //TODO: we have to break the method by using the keyword "return"so it will return a 403 to the unAuthorize user
         }
-//TODO: here is the Else part of the method where we extract to Jwt token from the headers
+
+    //TODO: here is the Else part of the method where we extract to Jwt token from the headers
 // T: we have to extract the Bearer and the white space from the token header that why we have the subString method
-jwt = authenticationHeader.substring(7);
-// TODO: now we have out the authentication token
+
+        jwt = authenticationHeader.substring(7);
+    // TODO: now we have our the authentication token
+
+    //TODO: where we extract our user email from  token
     }
 }
---
+++ boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/JWTservice/JwtService.java
@@ -0,0 +1,9 @@
+package com.example.boneshakerSecurity2.JWTservice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUserName(String userName) {return null;}
+}
