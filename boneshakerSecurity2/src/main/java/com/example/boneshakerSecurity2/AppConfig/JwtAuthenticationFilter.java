9dc0fc4983c48531202151ccdfb189df70677a35
samuelshola777 <samuelshola14@gmail.com>
2023-07-08 14:31:52 +0100

    creating the jwt filter

--- boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/AppConfig/JwtAuthenticationFilter.java
+++ boneshakerSecurity2/src/main/java/com/example/boneshakerSecurity2/AppConfig/JwtAuthenticationFilter.java
@@ -18,7 +18,9 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-
+// TODO: when we send a request we need to pass the authentication token withen the header
+        //TODO : to check we our request have jwt authentication token
+       //TODO : trying to extract the header called authorization
         final String authenticationHeader = request.getHeader("Authorization");
         final String jwt;
         if (authenticationHeader == null || authenticationHeader.startsWith("Bearer ")) {
